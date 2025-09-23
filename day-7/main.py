3ec6fb8f63dde7bbbf5c36c96c555a819d60d6bf
Mottuqe Brid <mottuqebrid@gmail.com>
2025-09-24 01:21:17 +0600

    Refactor day-7 main.py to demonstrate Python operators and add new main.py for day-8

--- day-7/main.py
+++ day-7/main.py
@@ -1,30 +1,10 @@
-# learnning python variables and data 
-a=1
-
-print(a)
-a=2
-print(a)
-b="mottuqe"
-print(b)
-
-# data types
-print(type(type(a)))
-print(type(type(type(a))))
-print(type(a))
-
-
-list=[1,2,3,["one","two"]]
-print(list)
-print(type(list))
-
-typle=(1,2,3,4)
-print(typle)
-print(type(typle))
-
-set={1,2,3,4}
-print(set)
-print(type(set))
-
-dict={"one":1,"two":2,"three":3}
-print(dict)
-print(type(dict))
+# ptyhon operator
+x=14
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
