157995ca29bcc263c19a69800cee76576dedf09d
Mottuqe Brid <mottuqebrid@gmail.com>
2025-09-26 17:33:39 +0600

    Add main.py for day-14 demonstrating Python if-else statements

---
+++ day-14/main.py
@@ -0,0 +1,11 @@
+# python if else
+
+a=int(input("Inter your age: "))
+print("your age is: ",a)
+
+if (a>=18):
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+print("thank you")
