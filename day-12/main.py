93cf7580509838c5a34576809e8a31aefa449596
Mottuqe Brid <mottuqebrid@gmail.com>
2025-09-26 16:48:46 +0600

    Add main.py for day-12 demonstrating Python string manipulation

---
+++ day-12/main.py
@@ -0,0 +1,9 @@
+# python string
+name="John, Doe, Jane, Smith, Alice, Bob"
+print(name)
+print(name[7:name.__len__()-1])
+print(len(name))
+print(name.__len__())
+print(name[0:3])
+print(name[-2:])
+print(name[:-2])
