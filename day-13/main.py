4ae48de39bf2bca1bf328dc5bb8db82e962fee4f
Mottuqe Brid <mottuqebrid@gmail.com>
2025-09-26 17:13:55 +0600

    Add main.py for day-13 demonstrating Python string methods

---
+++ day-13/main.py
@@ -0,0 +1,26 @@
+# python string methods
+name="!!!john!!!!!!!!"
+number="1234"
+
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(number.lower())
+print(name.capitalize())
+print(name.rstrip("!"))
+print(name.lstrip("!"))
+print(name.strip("!"))
+print(name.replace("!","#"))
+print(name.split("o"))
+
+title="python for beginners"
+print(title.capitalize())
+print(title.title())
+print(title.center(50))
+print(title.center(50,"8"))
+print(title.endswith("beginners"))
+print(title.find("for"))
+print(title.find("forfgh"))
+print(title.index("for"))
+print(title.isalnum())
+
