d96402c741fccd6658ed5bf9f351d35bb0892715
Mottuqe Brid <mottuqebrid@gmail.com>
2025-09-29 20:31:28 +0600

    Add main.py for day-18 demonstrating Python while loop

---
+++ day-18/main.py
@@ -0,0 +1,16 @@
+# python while loop
+t=0
+while t<=3:
+    print(t)
+    t+=1
+print("Done")
+# take input until user enters a number greater than 10
+# i=int(input("Enter a number: "))
+# while i<=10:
+#     i=int(input("Enter a number: "))
+#     print(i)
+# else:
+#     print("Number is greater than 10")
+
+print("Done")
+
