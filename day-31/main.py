d22c33e2aa26e61299342644de80d61b85af939e
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-07 22:30:14 +0600

    Add main.py for day-31 demonstrating set operations in Python

---
+++ day-31/main.py
@@ -0,0 +1,14 @@
+# sets in Python
+
+set1={2,3,4,5,6,3}
+print(set1)
+print(type(set1))
+set1.add(7)
+print(set1)
+set1.remove(2)
+print(set1)
+temp = set()
+print(type(temp))
+
+for i in set1:
+    print(i)
