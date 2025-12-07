b9f76fa03ea28e505da27e1ac6063e61117f9f6e
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-07 15:09:03 +0600

    docstring

---
+++ day-29/main.py
@@ -0,0 +1,19 @@
+# python docstring
+
+def square(n):
+    """
+    Returns the square of a number.
+
+    Parameters:
+    n (int or float): The number to be squared.
+
+    Returns:
+    int or float: The square of the input number.
+    """
+    return n * n
+# print(square.__doc__)
+# print(square(5))
+
+# PEP 8 - Python Enhancement Proposal
+# Zen of Python
+import this
