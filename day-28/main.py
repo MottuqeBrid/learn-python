e235d01f14b857c5a4e45b5ebedfff7d9d96b031
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-07 14:37:02 +0600

    learn f-string

---
+++ day-28/main.py
@@ -0,0 +1,8 @@
+# python f-strings
+letter="My name is {}, I am from {}"
+country = "Bangladesh"
+name = "Sahoreia"
+
+print(letter.format(name, country))
+print(f"My name is {name}, I am from {country}")
+
