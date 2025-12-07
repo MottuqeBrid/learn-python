197519734d2fe23d233460adff86c5c7475afa2d
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-07 23:06:21 +0600

    Add main.py for day-33 demonstrating Python dictionaries

---
+++ day-33/main.py
@@ -0,0 +1,14 @@
+# python dictionaries
+dic={
+    "name":"John",
+    "age":30,
+    "city":"New York",
+    "country":"USA",
+    "age":30,
+    "adult":True,
+    "skills":["Python","JavaScript","C++"]
+}
+print(dic)
+print(dic["name"])
+print(dic.keys())
+print(dic.values())
