ff1028e135a0fc311dc4cbf75d2220107d4e3a43
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-11 18:58:44 +0600

    Add main.py to demonstrate operator overloading with Vactor class in Python

---
+++ day-77/main.py
@@ -0,0 +1,22 @@
+# operator overloading in Python
+
+
+class Vactor:
+    def __init__(self, i, j, k):
+        self.i = i
+        self.j = j
+        self.k = k
+
+    def __str__(self):
+        return f"{self.i}i + {self.j}j + {self.k}k"
+
+    def __add__(self, x):
+        return Vactor(self.i + x.i, self.j + x.j, self.k + x.k)
+
+
+v = Vactor(2, 3, 4)
+print(v)
+v2 = Vactor(5, 6, 7)
+print(v2)
+print(v + v2)
+print(type(v + v2))
