d33db767236513036e25edd71092e5ec6f423822
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-11 23:59:59 +0600

    Add main.py to demonstrate the usage of the shutil module in Python

---
+++ day-87/main.py
@@ -0,0 +1,15 @@
+# Shutil module in Python
+import shutil
+import os
+
+# shutil.copy("day-87/main.py", "day-87/main-2.py")
+
+# shutil.copytree("day-71", "day-87/day-71-backup")
+
+# shutil.move("day-87/main-2.py", "day-87/main-moved.py")
+
+# for deleting files
+# shutil.rmtree("day-87/day-71-backup")
+
+# for deleting single file
+# os.remove("day-87/main-moved.py")
