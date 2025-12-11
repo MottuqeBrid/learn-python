b420e7fe2bdc6a005b6605f06a858b08772d1bb4
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-11 22:37:37 +0600

    Refactor code structure for improved readability and maintainability

--- day-82/main.py
+++ day-82/main.py
@@ -1,1 +1,14 @@
-# multulevel inheritance in python
+# Marge the PDF files into a single PDF file
+from PyPDF2 import PdfWriter
+import os
+
+
+merger = PdfWriter()
+
+files = [file for file in os.listdir() if file.endswith(".pdf")]
+
+for pdf in files:
+    merger.append(pdf, import_outline=False)
+
+merger.write("merged-pdf.pdf")
+merger.close()
