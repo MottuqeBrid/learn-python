5120bd08a4f40953805cf9cf68a7376da30cc161
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-11 22:07:10 +0600

    Add main.py to demonstrate hybrid and hierarchical inheritance in Python

---
+++ day-81/main.py
@@ -0,0 +1,17 @@
+# Hybrid and Hirarchical Inheritance in Python
+
+
+class BaseClass:
+    pass
+
+
+class Drtive1(BaseClass):
+    pass
+
+
+class Drtive2(BaseClass):
+    pass
+
+
+class Hybrid(Drtive1, Drtive2):
+    pass
